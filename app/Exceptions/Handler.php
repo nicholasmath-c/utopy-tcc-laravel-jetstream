0488aa23c26346ebbeef50e3cf2c9964731d40ca
Etec <etec@etec.com>
2023-05-12 20:07:09 -0300

    arrumando composer error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,16 +1,50 @@
 <?php
+
 namespace App\Exceptions;
-use Arremessável; 
-use Illuminate\Foundation\Exceptions\Handler como ExceptionHandler;
-class Handler extends ExceptionHandler 
-{ 
-   // use Throwable - você NÃO deve importar a classe Throwable como uma característica aqui. Você precisa apenas importá-lo acima da classe
-    public function report(Throwable $exception) 
-    { 
-        pai::report($exception); 
-    }
-    public function render($request, Throwable $exception) 
-    { 
-        return parent::render($request, $exception); 
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of exception types with their corresponding custom log levels.
+     *
+     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     */
+    protected $levels = [
+        //
+    ];
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<\Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
     }
 }
