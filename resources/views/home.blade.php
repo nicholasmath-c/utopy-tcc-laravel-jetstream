e7fec5510dda2aec9405f5ccd13046c21fce6e18
Nicholas M. A. Costa <nicholasmatheus.costa@hotmail.com>
2023-05-13 19:37:20 -0300

    trocando a welcome page para a login page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-white leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Home') }}
         </h2>
     </x-slot>
 
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,14 @@ use App\Http\Controllers\Game\GenreGameController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/', function () {
+        return view('home');
+    })->name('home');
 });
 
 
