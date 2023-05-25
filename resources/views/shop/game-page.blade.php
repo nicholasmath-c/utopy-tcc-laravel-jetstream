74150f079e5f8074daa66257af7005b4604bdc5e
Deivid Lima Souza <110212061+WaterCooler727@users.noreply.github.com>
2023-05-25 18:32:24 -0300

    FrontEnd - continuando GamePage
    descrição do jogo, ajustando posicionamentos, ajustando imagens, adicionando div no canto inferior direito para se nescessario usar

--- resources/views/shop/game-page.blade.php
+++ resources/views/shop/game-page.blade.php
@@ -6,35 +6,59 @@
 
                 <div class=" max-w-4xl pr-4">
                     <h1 class=" text-white text-4xl">Celeste</h1>
-                    <img src="{{ url('img/games_img/CelesteBanner.jpeg') }}">
+                    <img class=" rounded-md " src="{{ url('img/games_img/CelesteBanner.jpeg') }}">
                 </div>
 
-                <div class="items-center pt-16">
-                    <img class="max-w-xs" src="{{ url('img/games_img/CelesteBanner.jpeg') }}" >
+                <div class="items-center pt-20">
+                    <img class="max-w-xs rounded-md " src="{{ url('img/games_img/CelesteBanner.jpeg') }}">
                     <div class="pt-4">
-                        <p class="flex flex-row text-white text-lg"> <span class=" text-gray-500">Desenvolvedor -</span>  <span class=" pl-6" > Cherry </span>   </p>
-                        <hr>
-                        <p class="text-white text-lg"> <span class=" text-gray-500">Categoria -</span> <span class=" pl-6" > Aventura </span>   </p>
-                        <hr>
-                        <div class=" mt-10 ml-9">
+                        <p class="flex flex-row text-white text-lg"> <span class=" text-gray-500">Desenvolvedor -</span>
+                            <span class=" pl-6"> Cherry </span>
+                        </p>
+                        <hr class=" rounded-md" >
+                        <p class="text-white text-lg"> <span class=" text-gray-500">Categoria -</span> <span
+                                class=" pl-6"> Aventura </span> </p>
+                        <hr class=" rounded-md" >
 
+                        <div class=" mt-10 ml-9">
                             <x-button class=" w-64 h-16"> Adcionar ao Carrinho </x-button>
                             <x-button class=" mt-4 w-64 h-16"> Comprar Jogo </x-button>
-
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class=" text-white max-w-full mt-5  ">
+                <h1 class=" text-3xl ">Sobre este jogo</h1>
 
+                <div class="flex flex-row w-full">
+                    <div class=" max-w-3xl " >
+                        <p class=" text-lg ">
+                            Esse jogo é uma aventura com narrativa de um jogador, tal como a mamã costumava fazer, com um
+                            elenco
+                            de personagens encantadoras e uma história emocionante de auto-descoberta.
+                            Uma enorme montanha com mais de 600 ecrãs de complicados desafios de plataformas e segredos
+                            perturbadores.
+                            Capítulos paralelos brutais para desbloquear, criados apenas para os montanhistas mais
+                            corajosos.
+                            Finalista IGF no prémio "Excelência no Áudio", com mais de 2 horas de música original
+                            orquestrada
+                            por piano deslumbrante e ritmos sintéticos cativantes
+                            Torta. Controlos simples e acessíveis - saltar, trepar e rajada - mas com pormenores de mestria
+                            onde
+                            cada morte é uma lição. Com ressurgimentos ultra-rápidos, não irás parar de trepar enquanto
+                            descobres os mistérios da montanha e enfrentas os seus muitos perigos.
+                        </p>
+                    </div>
+                    <div class=" w-full border-l-4 border-main-500 rounded-sm pl-4 ml-4 ">
+                        <p class=" text-lg  "></p>
                     </div>
 
                 </div>
+
+
+
+
             </div>
-
-
-        </div>
-
-
-
-
-    </div>
 
 
 
