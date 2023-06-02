09b8d96032bf80bf09a07e6e9b2c559a6f9dac82
Etec <110212061+WaterCooler727@users.noreply.github.com>
2023-06-01 22:03:17 -0300

    FrontEnd- detalhando ShopCart( Little Car )
    buckle my shoes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,13 +1,13 @@
 <x-admin-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-slate-800 overflow-hidden shadow-xl sm:rounded-lg">
 
             </div>
         </div>
     </div>
-    </div>
-    </div>
-    </div>
+
+
+
 
 </x-admin-layout>
