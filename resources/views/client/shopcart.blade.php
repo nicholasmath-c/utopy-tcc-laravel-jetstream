6455f6c7290c65f71386226a4f70e05fff23211a
Ronald Ribeiro <110943139+ribeiroronald@users.noreply.github.com>
2023-06-02 14:24:20 -0300

    Front-end Carrinho de Compras

--- resources/views/client/shopcart.blade.php
+++ resources/views/client/shopcart.blade.php
@@ -1,7 +1,13 @@
 <x-app-layout>
 
 
-    <h1 class=" pl-10 pt-5 font-heading text-4xl text-white ">Meu Carrinho</h1>
+    <div class="flex justify-between mt-10">
+        <h1 class=" pl-10 pt-5 font-heading text-4xl text-white ">Meu Carrinho</h1>
+        <h1 class="pl-10 pr-8 pt-5 font-heading text-3xl text-white mt-0">Resumo de Jogos e Aplicativos</h1> 
+        
+      </div>
+
+    
 
     <div class=" mb-12 flex flex-col md:flex-row max-w-7xl mx-auto  ">
 
@@ -85,55 +91,53 @@
 
 
 
-            <div class=" flex flex-col h-60 ">
-
-                <div class=" flex flex-col items-center mt-10">
-                    <h1 class="underline underline-offset-4  text-lg  mt-10"> Valor Total: <span class=" text-lg text-red-300 "> R$80,00 </span>  </h1>
-                </div>
-
-                <div class=" flex flex-row mt-8 justify-center ">
-                    <div class="  ">
-                        <x-button class=" h-12 w-32" > Comprar </x-button>
-                    </div>
-
-                    <div class=" ml-7">
-                        <x-button class=" h-12 w-32" > Presentear </x-button>
-                    </div>
-
-                </div>
-
-            </div>
+            
 
         </div>
 
-
-
-
-
-        <div class=" md:border-l-2  md:mt-20 flex flex-col w-full md:w-5/12  border-0 border-main-500 pl-2 mt-5 text-white">
-
-
-            <div class=" flex flex-col h-full p-10">
-                <h1 class=" self-center text-xl">Recomendado para você</h1>
-                <div class=" p-1 rounded-md border-2 border-slate-800 hover:border-red-500 ease-in-out duration-150 mb-4">
-                    <img class="rounded-sm" src="{{ url('img/games_img/OriBanner.jpg') }}" alt="">
-                    <div class=" bg-opacity-20 rounded-sm bg-slate-900" >
-                        <h1 class="text-lg" >R$30,00   <span class=" text-sm line-through text-gray-500 "> R$50,00 </span>  </h1>
-                    </div>
-
+<div class="md:border-l-2 md:mt-10 flex flex-col w-full md:w-5/12 border-0 border-main-500 pl-2 mt-5 text-white mt-0">
+    <div class="flex flex-col h-full p-5 mt-0">
+        <div class="flex flex-col h-70">
+            <div class="flex flex-col items-left mt-20 mt-0">
+                    <h1 class="pl-10 pt-5 font-heading text-4xl text-white mt-0 ">Resumo de Jogos e Aplicativos</h1>
+                <div class="flex justify-between mt-10 mt-0">
+                    <h1 class="text-lg mt-3">Preço</h1>
+                    <h1 class="text-lg mt-3"><span class="text-lg text-red-300">R$ 80,00</span></h1>
                 </div>
-
-                <div class=" p-1 rounded-md border-2 border-slate-800 hover:border-red-500 ease-in-out duration-150 mb-4">
-                    <img class="rounded-sm" src="{{ url('img/games_img/CelesteBanner.jpeg') }}" alt="">
-                    <div class=" bg-opacity-20 rounded-sm bg-slate-900" >
-                        <h1 class="text-lg" >R$30,00   <span class=" text-sm line-through text-gray-500 "> R$50,00 </span>  </h1>
-                    </div>
-
+                <div class="flex justify-between mt-0">
+                    <h1 class="text-lg mt-3">Desconto</h1>
+                    <h1 class="text-lg mt-3"><span class="text-lg text-red-300">R$ 30,00</span></h1>
                 </div>
-
+                <div class="flex justify-between mt-4 border-t">
+                    <h1 class="text-lg mt-4">Subtotal</h1>
+                    <h1 class="text-lg mt-4"><span class="text-lg text-red-300">R$ 80,00</span></h1>
+                </div>
             </div>
+            <div class="flex flex-row mt-8 justify-center mt-0">
+                <div>
+                    <x-button class="h-12 w-60">Finalizar Compra</x-button>
+                </div>
+            </div>
+        </div>
+        <h1 class="self-center text-xl mt-10">Recomendado para você</h1>
+        <div class="p-1 rounded-md border-2 border-slate-800 hover:border-red-500 ease-in-out duration-150 mb-4">
+            <img class="rounded-sm" src="{{ url('img/games_img/OriBanner.jpg') }}" alt="">
+            <div class="bg-opacity-20 rounded-sm bg-slate-900">
+                <h1 class="text-lg">R$20,00   <span class="text-sm line-through text-gray-500">R$50,00</span></h1>
+            </div>
+        </div>
+        <div class="p-1 rounded-md border-2 border-slate-800 hover:border-red-500 ease-in-out duration-150 mb-4">
+            <img class="rounded-sm" src="{{ url('img/games_img/CelesteBanner.jpeg') }}" alt="">
+            <div class="bg-opacity-20 rounded-sm bg-slate-900">
+                <h1 class="text-lg">R$20,00   <span class="text-sm line-through text-gray-500">R$50,00</span></h1>
+            </div>
+        </div>
+    </div>
+</div>
+
 
         </div>
+            
     </div>
 
 
