aca414b38dfdcfe5e93f496d6d54af1974de7723
Etec <110212061+WaterCooler727@users.noreply.github.com>
2023-06-06 20:45:24 -0300

    Front-End  Startando Library
    div vertical de jogos

--- resources/views/client/library.blade.php
+++ resources/views/client/library.blade.php
@@ -1,5 +1,61 @@
 <x-app-layout>
 
+    <div class=" text-white border-2 flex flex-col p-6">
 
+            <div class=" border-2 border-yellow-400 h-7" >
+
+            </div>
+
+
+            <div class=" flex flex-col border-2 border-red-600  p-6" >
+
+            <div class=" flex flex-row items-center border-2 border-green-500 h-44 p-3">
+                    <img class=" h-36 " src="{{ url('img/games_img/CelesteCape.png') }}" alt="">
+                    <div class=" ml-2 border-2 h-full w-full flex flex-col ">
+
+                        <div class=" mb-1 border-2 border-red-500 ">
+                            <h1 class=" font-heading text-white text-2xl " >Celeste</h1>
+                        </div>
+
+                        <div class=" flex w-fill mb-1 border-2 border-red-500 ">
+
+                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                                <h1 class=" text-white text-base " >Aventura</h1>
+                            </div>
+
+                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                                <h1 class=" text-white text-base " >Plataforma</h1>
+                            </div>
+
+                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                                <h1 class=" text-white text-base " >8-bits</h1>
+                            </div>
+
+                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                                <h1 class=" text-white text-base " >Aventura</h1>
+                            </div>
+
+                        </div>
+
+                        <div class=" flex w-fill mb-1 border-2 border-red-500 " >
+                            <h1>Lançamento -</h1>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+                <div class=" flex flex-row items-center border-2 border-green-500 h-44 p-3">
+                    <img class=" h-36 " src="{{ url('img/games_img/CelesteCape.png') }}" alt="">
+
+                </div>
+
+
+
+
+            </div>
+
+    </div>
 
 </x-app-layout>
