7dcc058e977d1cb582605374fc70d637e0154eb4
Etec <110212061+WaterCooler727@users.noreply.github.com>
2023-06-06 22:02:00 -0300

    front end library

--- resources/views/client/library.blade.php
+++ resources/views/client/library.blade.php
@@ -11,34 +11,37 @@
 
             <div class=" flex flex-row items-center border-2 border-green-500 h-44 p-3">
                     <img class=" h-36 " src="{{ url('img/games_img/CelesteCape.png') }}" alt="">
-                    <div class=" ml-2 border-2 h-full w-full flex flex-col ">
+                    <div class=" flex flex-col justify-center ml-2 border-2 h-full w-full  ">
 
-                        <div class=" mb-1 border-2 border-red-500 ">
+                        <div class=" mb-1 ">
                             <h1 class=" font-heading text-white text-2xl " >Celeste</h1>
                         </div>
 
-                        <div class=" flex w-fill mb-1 border-2 border-red-500 ">
+                        <div class=" flex w-fill mb-1 ">
 
-                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                            <div class=" p-1 mr-1 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
                                 <h1 class=" text-white text-base " >Aventura</h1>
                             </div>
 
-                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                            <div class=" p-1 mr-1 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
                                 <h1 class=" text-white text-base " >Plataforma</h1>
                             </div>
 
-                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                            <div class=" p-1 mr-1 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
                                 <h1 class=" text-white text-base " >8-bits</h1>
                             </div>
 
-                            <div class=" p-1 mr-1 border-2 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
+                            <div class=" p-1 mr-1 border-gray-500 rounded-xl bg-slate-600 bg-opacity-25 ">
                                 <h1 class=" text-white text-base " >Aventura</h1>
                             </div>
 
                         </div>
 
-                        <div class=" flex w-fill mb-1 border-2 border-red-500 " >
-                            <h1>Lançamento -</h1>
+                        <div class=" flex items-center w-fill mb-1  " >
+                            <h1 class="mr-5" >Lançamento -  <span class=" text-lg ">21/21/21 </span>     </h1>
+                            <div class="w-8 h-8  ">
+                                <img src="{{ url('storage/img/classificacoes/classificacao-livre.png') }}" alt="">
+                            </div>
                         </div>
 
 
