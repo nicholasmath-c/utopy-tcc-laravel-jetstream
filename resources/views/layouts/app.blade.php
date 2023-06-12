e95316f5878480c11a076771cc39263a3a16c90b
Deivid Lima Souza <110212061+WaterCooler727@users.noreply.github.com>
2023-06-12 17:44:45 -0300

    FrontEnd continuando library
    finalizando slots de games, e começando barra de categorias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,8 @@
             <!-- Page Content -->
             <main>
                 <div class="py-12">
-                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div class="bg-slate-800 overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class=" max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="text-white bg-slate-800 overflow-hidden shadow-xl sm:rounded-lg">
                             {{ $slot }}
                         </div>
                     </div>
