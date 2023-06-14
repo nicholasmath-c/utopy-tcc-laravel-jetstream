d1a5c861abf456eb6a9f76d5c1ed8349e66c20c4
ribeiroronald <ribeiroronald627@gmail.com>
2023-06-14 06:05:58 -0300

    Alinhamento do bloco " resumo de jogos"

--- resources/views/client/shopcart.blade.php
+++ resources/views/client/shopcart.blade.php
@@ -2,8 +2,8 @@
 
 
     <div class="flex justify-between mt-10">
-        <h1 class=" pl-10 pt-5 font-heading text-4xl text-white ">Meu Carrinho</h1>
-        <h1 class="pl-10 pr-8 pt-5 font-heading text-3xl text-white mt-0">Resumo de Jogos e Aplicativos</h1>
+        <h1 class=" pl-10 pt- font-he1ading text-3xl text-white ">Meu Carrinho</h1>
+        
 
       </div>
 
@@ -14,17 +14,20 @@
 
         <div class="flex flex-col md:w-7/12 w-full text-white">
 
-            <div class=" flex h-80 items-center ">
+            <div class=" flex h-80   items-center ">
 
                 <div class=" flex flex-col h-72 w-full overflow-auto scrollbar-hide   ">
 
                     <div class=" mb-2 pl-10 pb-3 pr-28">
-                        <div class=" rounded-md border-2 border-slate-800 hover:border-red-500 ease-in-out duration-150   pl-2 flex flex-row pt-2">
+                        <div class=" rounded-md border-2 border-slate-600 hover:border-red-500 ease-in-out duration-150   pl-3 flex flex-row pt-20">
 
-                            <img class=" max-h-20 rounded-sm mb-2 " src="{{ url('img/games_img/CelesteCape.png') }}" alt="">
+                            <img class=" max-h-20 rounded-sm mb-12 " src="{{ url('img/games_img/CelesteCape.png') }}" alt="">
                                 <div class="self-center w-96">
-                                    <h1 class=" ml-2" > R$ 20,00</h1>
-                                    <h1> <span class=" ml-2 text-gray-400" >Jogo base - </span>  Celeste</h1>
+                                <x-button class=" mb-20 ml-3 h-6 w-15">JOGO BASE</x-button>
+                                <h1> <span class=" mb-15 ml-3 text-white-400" >Celeste</span></h1>
+                                    <h1 class=" ml-2 " > R$ 20,00</h1>
+                                    
+                                    
                                 </div>
 
                                 <div class="flex flex-col self-end items-center pb-2 ">
@@ -95,11 +98,11 @@
 
         </div>
 
-<div class="md:border-l-2 md:mt-10 flex flex-col w-full md:w-5/12 border-0 border-main-500 pl-2 text-white mt-0">
+<div class="md:border-l-2 md:mt-1 flex flex-col w-full md:w-5/12 border-0 border-main-500 pl-2 text-white mt-0">
     <div class="flex flex-col h-full p-5 mt-0">
         <div class="flex flex-col h-70">
-            <div class="flex flex-col items-left mt-20 ">
-                    <h1 class="pl-10 pt-5 font-heading text-4xl text-white mt-0 ">Resumo de Jogos e Aplicativos</h1>
+            <div class="flex flex-col items-left mt-1 ">
+                    <h1 class="pl-10 pt-1 font-heading text-4xl text-white mt-0 ">Resumo de Jogos e Aplicativos</h1>
                 <div class="flex justify-between mt-10 ">
                     <h1 class="text-lg mt-3">Preço</h1>
                     <h1 class="text-lg mt-3"><span class="text-lg text-red-300">R$ 80,00</span></h1>
@@ -115,8 +118,17 @@
             </div>
             <div class="flex flex-row mt-8 justify-center ">
                 <div>
-                    <x-button class="h-12 w-60">Finalizar Compra</x-button>
+                    <x-button class="h-55 w-55">Finalizar Compra</x-button>
+                   
                 </div>
+
+                <span style="margin-left: 10px;"></span>
+
+                <div>
+                <x-button class="h-55 w-50">Continuar comprando</x-button> 
+
+                </div>
+                
             </div>
         </div>
         <h1 class="self-center text-xl mt-10">Recomendado para você</h1>
