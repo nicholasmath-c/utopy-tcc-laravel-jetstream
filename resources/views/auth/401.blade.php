f75e1e9a4d249011fa2253fe2c40db823edfb13c
Deivid Lima Souza <110212061+WaterCooler727@users.noreply.github.com>
2023-06-15 22:41:02 -0300

    FrontEnd - 401page
    Creating 401 error page, and switching svg image to png format

--- resources/views/auth/401.blade.php
+++ resources/views/auth/401.blade.php
@@ -1,14 +1,29 @@
-<x-admin-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-slate-900 p-10 overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="font-heading font-bold text-4xl">
-                    401
-                </div>
-            </div>
+<x-simplelay-layout>
+    <div class="flex flex-row justify-center">
+
+        <div class="py-14">
+            <img src="{{ url('img\utopy\floatingIsland.png') }}" alt=""
+                class="  animate-updown ease-in-out self-center">
         </div>
+
+        <div class="flex flex-col items-center pt-56 w-2/5 ">
+
+            <div class="font-heading text-4xl text-slate-700 mb-2">
+                401
+            </div>
+
+            <div class="font-heading text-4xl text-main-500 mb-10">
+                Oops... algo deu errado
+            </div>
+
+            <div class=" text-xl">
+                Você não possui autorização para acessar essa página
+            </div>
+
+        </div>
+
+
     </div>
-    </div>
-    </div>
-    </div>
-</x-admin-layout>
+
+
+</x-simplelay-layout>
