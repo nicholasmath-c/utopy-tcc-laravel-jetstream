4c79c4be13f9627ab4b596564a044eceadd6411d
ribeiroronald <ribeiroronald627@gmail.com>
2023-06-19 12:52:04 -0300

    inserindo os demais titulos no carrinho

--- resources/views/client/shopcart.blade.php
+++ resources/views/client/shopcart.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
 
-    <div class="flex justify-between mt-10">
+    <div class="flex justify-between ">
         <h1 class=" pl-10 pt- font-he1ading text-3xl text-white mb-12 ">Meu Carrinho</h1>
 
 
@@ -24,7 +24,7 @@
 
                             <div class="flex flex-col justify-center  h-32 w-32">
 
-                                <img class=" object-contain h-32 rounded-sm  "
+                                <img class=" object-contain h-32 mt-5 rounded-sm  "
                                 src="{{ url('storage/games/Celeste/Celeste-Cover.png') }}" alt="">
 
                             </div>
@@ -38,7 +38,7 @@
 
                                 <div class=" pt-6 w-52">
                                     <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
-                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Silksong</h1>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Celeste</h1>
 
 
                                 </div>
@@ -68,46 +68,7 @@
                             <div class="flex flex-col justify-center  h-32 w-32">
 
                                 <img class=" object-contain h-32 rounded-sm  "
-                                src="{{ url('storage/games/Celeste/Celeste-Cover.png') }}" alt="">
-
-                            </div>
-
-                            <div class=" w-96">
-
-
-
-
-
-
-                                <div class=" pt-6 w-52">
-                                    <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
-                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Silksong</h1>
-
-
-                                </div>
-
-                            </div>
-
-                            <div class="flex flex-col self-end items-center pt-6 pb-2 ">
-                                <h1 class=" mr-10 text-gray-500 text-lg line-through ">R$40,00</h1>
-                                <h1 class="  mr-11 ml-3 text-1xl ">R$20,00</h1>
-                                <a class=" mt-10 text-sm underline text-gray-500 hover:text-red-500 mr-2 "
-                                    href="">Remover</a>
-                                <hr class=" mt-2">
-                            </div>
-                        </div>
-
-                    </div>
-
-
-
-                    <div class="  mb-10 pl-10 pb-3 pr-28">
-                        <div class="rounded-md border-2 p-2 bg-gray-900 border-slate-600 hover:border-red-500 ease-in-out duration-150  flex flex-row ">
-
-                            <div class="flex flex-col justify-center  h-32 w-32">
-
-                                <img class=" object-contain h-32 rounded-sm  "
-                                src="{{ url('storage/games/Celeste/Celeste-Cover.png') }}" alt="">
+                                src="{{ url('storage/games/Hollow Knight - SilkSong/SilkSong-Cover.png') }}" alt="">
 
                             </div>
 
@@ -146,7 +107,7 @@
                             <div class="flex flex-col justify-center  h-32 w-32">
 
                                 <img class=" object-contain h-32 rounded-sm  "
-                                src="{{ url('storage/games/Celeste/Celeste-Cover.png') }}" alt="">
+                                src="{{ url('storage/games/Cuphead/Cuphead-Cover.png') }}" alt="">
 
                             </div>
 
@@ -159,7 +120,7 @@
 
                                 <div class=" pt-6 w-52">
                                     <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
-                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Silksong</h1>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Cuphead</h1>
 
 
                                 </div>
@@ -176,6 +137,238 @@
                         </div>
 
                     </div>
+
+
+
+                    <div class="  mb-10 pl-10 pb-3 pr-28">
+                        <div class="rounded-md border-2 p-2 bg-gray-900 border-slate-600 hover:border-red-500 ease-in-out duration-150  flex flex-row ">
+
+                            <div class="flex flex-col justify-center  h-32 w-32">
+
+                                <img class=" object-contain h-32 rounded-sm  "
+                                src="{{ url('storage/games/Dredge/Dredge-Cover.png') }}" alt="">
+
+                            </div>
+
+                            <div class=" w-96">
+
+
+
+
+
+
+                                <div class=" pt-6 w-52">
+                                    <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Dredge</h1>
+
+
+                                </div>
+
+                            </div>
+
+                            <div class="flex flex-col self-end items-center pt-6 pb-2 ">
+                                <h1 class=" mr-10 text-gray-500 text-lg line-through ">R$40,00</h1>
+                                <h1 class="  mr-11 ml-3 text-1xl ">R$20,00</h1>
+                                <a class=" mt-10 text-sm underline text-gray-500 hover:text-red-500 mr-2 "
+                                    href="">Remover</a>
+                                <hr class=" mt-2">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="  mb-10 pl-10 pb-3 pr-28">
+                        <div class="rounded-md border-2 p-2 bg-gray-900 border-slate-600 hover:border-red-500 ease-in-out duration-150  flex flex-row ">
+
+                            <div class="flex flex-col justify-center  h-32 w-32">
+
+                                <img class=" object-contain h-32 rounded-sm  "
+                                src="{{ url('storage/games/Five Nights at Freddys- Sister Location/SisterLocation-Cover.png') }}" alt="">
+
+                            </div>
+
+                            <div class=" w-96">
+
+
+
+
+
+
+                                <div class=" pt-6 w-52">
+                                    <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Five Nights at Freddy's- Sister Location</h1>
+
+
+                                </div>
+
+                            </div>
+
+                            <div class="flex flex-col self-end items-center pt-6 pb-2 ">
+                                <h1 class=" mr-10 text-gray-500 text-lg line-through ">R$40,00</h1>
+                                <h1 class="  mr-11 ml-3 text-1xl ">R$20,00</h1>
+                                <a class=" mt-10 text-sm underline text-gray-500 hover:text-red-500 mr-2 "
+                                    href="">Remover</a>
+                                <hr class=" mt-2">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="  mb-10 pl-10 pb-3 pr-28">
+                        <div class="rounded-md border-2 p-2 bg-gray-900 border-slate-600 hover:border-red-500 ease-in-out duration-150  flex flex-row ">
+
+                            <div class="flex flex-col justify-center  h-32 w-32">
+
+                                <img class=" object-contain h-32 rounded-sm  "
+                                src="{{ url('storage/games/Inside/Inside-Cover.png') }}" alt="">
+
+                            </div>
+
+                            <div class=" w-96">
+
+
+
+
+
+
+                                <div class=" pt-6 w-52">
+                                    <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Inside</h1>
+
+
+                                </div>
+
+                            </div>
+
+                            <div class="flex flex-col self-end items-center pt-6 pb-2 ">
+                                <h1 class=" mr-10 text-gray-500 text-lg line-through ">R$40,00</h1>
+                                <h1 class="  mr-11 ml-3 text-1xl ">R$20,00</h1>
+                                <a class=" mt-10 text-sm underline text-gray-500 hover:text-red-500 mr-2 "
+                                    href="">Remover</a>
+                                <hr class=" mt-2">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="  mb-10 pl-10 pb-3 pr-28">
+                        <div class="rounded-md border-2 p-2 bg-gray-900 border-slate-600 hover:border-red-500 ease-in-out duration-150  flex flex-row ">
+
+                            <div class="flex flex-col justify-center  h-32 w-32">
+
+                                <img class=" object-contain h-32 rounded-sm  "
+                                src="{{ url('storage/games/Limbo/Limbo-Cover.png') }}" alt="">
+
+                            </div>
+
+                            <div class=" w-96">
+
+
+
+
+
+
+                                <div class=" pt-6 w-52">
+                                    <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Limbo</h1>
+
+
+                                </div>
+
+                            </div>
+
+                            <div class="flex flex-col self-end items-center pt-6 pb-2 ">
+                                <h1 class=" mr-10 text-gray-500 text-lg line-through ">R$40,00</h1>
+                                <h1 class="  mr-11 ml-3 text-1xl ">R$20,00</h1>
+                                <a class=" mt-10 text-sm underline text-gray-500 hover:text-red-500 mr-2 "
+                                    href="">Remover</a>
+                                <hr class=" mt-2">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="  mb-10 pl-10 pb-3 pr-28">
+                        <div class="rounded-md border-2 p-2 bg-gray-900 border-slate-600 hover:border-red-500 ease-in-out duration-150  flex flex-row ">
+
+                            <div class="flex flex-col justify-center  h-32 w-32">
+
+                                <img class=" object-contain h-32 rounded-sm  "
+                                src="{{ url('storage/games/Ori and the Blind Forest/Ori-Thumb.jpeg') }}" alt="">
+
+                            </div>
+
+                            <div class=" w-96">
+
+
+
+
+
+
+                                <div class=" pt-6 w-52">
+                                    <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Ori and the Blind Forest</h1>
+
+
+                                </div>
+
+                            </div>
+
+                            <div class="flex flex-col self-end items-center pt-6 pb-2 ">
+                                <h1 class=" mr-10 text-gray-500 text-lg line-through ">R$40,00</h1>
+                                <h1 class="  mr-11 ml-3 text-1xl ">R$20,00</h1>
+                                <a class=" mt-10 text-sm underline text-gray-500 hover:text-red-500 mr-2 "
+                                    href="">Remover</a>
+                                <hr class=" mt-2">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="  mb-10 pl-10 pb-3 pr-28">
+                        <div class="rounded-md border-2 p-2 bg-gray-900 border-slate-600 hover:border-red-500 ease-in-out duration-150  flex flex-row ">
+
+                            <div class="flex flex-col justify-center  h-32 w-32">
+
+                                <img class=" object-contain h-32 rounded-sm  "
+                                src="{{ url('storage/games/Undertale/Undertale-Cover.png') }}" alt="">
+
+                            </div>
+
+                            <div class=" w-96">
+
+
+
+
+
+
+                                <div class=" pt-6 w-52">
+                                    <x-button class=" ml-3 h-6 w-15">JOGO BASE</x-button>
+                                    <h1 class="mb-15 ml-3 text-white-400 text-xl font-bold">Undertale</h1>
+
+
+                                </div>
+
+                            </div>
+
+                            <div class="flex flex-col self-end items-center pt-6 pb-2 ">
+                                <h1 class=" mr-10 text-gray-500 text-lg line-through ">R$40,00</h1>
+                                <h1 class="  mr-11 ml-3 text-1xl ">R$20,00</h1>
+                                <a class=" mt-10 text-sm underline text-gray-500 hover:text-red-500 mr-2 "
+                                    href="">Remover</a>
+                                <hr class=" mt-2">
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
 
 
                 </div>
