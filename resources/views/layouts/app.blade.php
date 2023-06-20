42055aad962ffe5bb2e5776d88660e5ac73cbc3a
Etec <59261158+Mus4ng@users.noreply.github.com>
2023-06-20 19:34:07 -0300

    Colocando o Footer da app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,5 +44,67 @@
         @stack('modals')
 
         @livewireScripts
+
+        <footer>
+            <div class="footer-column">
+              <h3>Navegação</h3>
+              <ul>
+                <li>Aventura</li>
+                <li>Terror</li>
+              </ul>
+            </div>
+            <div class="footer-column">
+              <h3>Redes Sociais</h3>
+              <ul>
+                <li>Instagram</li>
+                <li>Twitter</li>
+                <li>Facebook</li>
+              </ul>
+            </div>
+            <div class="footer-column">
+              <h3>Conta</h3>
+              <ul>
+                <li>Criar conta</li>
+                <li>Login</li>
+              </ul>
+            </div>
+            <div class="footer-bottom">
+              <p>&copy; 2023 Utopy - Plataforma de Jogos Indies. Todos os direitos reservados.</p>
+            </div>
+          </footer>
     </body>
 </html>
+
+<style>
+    /* Estilos CSS para o footer */
+    footer {
+      background-color: #0f172a	;
+      padding: 20px;
+      text-align: center;
+    }
+
+    .footer-column {
+      display: inline-block;
+      vertical-align: top;
+      margin-right: 250px;
+    }
+
+    .footer-column h3 {
+      font-size: 18px;
+      margin-bottom: 10px;
+    }
+
+    .footer-column ul {
+      list-style: none;
+      padding: 0;
+    }
+
+    .footer-column li {
+      margin-bottom: 50px;
+    }
+
+    .footer-bottom {
+      font-size: 14px;
+      color: white ;
+    }
+  </style>
