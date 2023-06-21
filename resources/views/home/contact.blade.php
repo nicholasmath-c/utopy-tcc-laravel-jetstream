e87376b73f89391f7990c80fed052ec79acd70bd
Etec <ribeiroronald627@gmail.com>
2023-06-20 22:14:41 -0300

    inicio das paginas de contato e sobre

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,6 +1,34 @@
 <x-app-layout>
-    <div class="mx-auto sm:px-6 lg:px-8 mb-10">
-        Contato
+    <div class="mx-auto sm:px-6 lg:px-8 mt-10  mb-10">
+
+        <div class=" bg-slate-200 mt-6 lg:mx-6 lg:w-1/2">
+            <div class="mx-auto w-full overflow-hidden rounded-xl bg-white px-8 py-10 shadow-2xl dark:bg-gray-900 lg:max-w-xl">
+            <h1 class="text-2xl font-medium text-gray-700 dark:text-gray-200">Contate-nos</h1>
+
+            <form class="mt-6">
+                <div class="flex-1">
+                <label class="mb-2 block text-sm text-gray-600 dark:text-gray-200">Nome completo</label>
+                <input type="text" placeholder="Digite seu nome" class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+                </div>
+
+                <div class="mt-6 flex-1">
+                <label class="mb-2 block text-sm text-gray-600 dark:text-gray-200">Email de contato</label>
+                <input type="email" placeholder="Endereço de email" class="mt-2 block w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+                </div>
+
+                <div class="mt-6 w-full">
+                <label class="mb-2 block text-sm text-gray-600 dark:text-gray-200">Mensagem</label>
+                <textarea class="mt-2 block h-32 w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 placeholder-gray-400 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300 md:h-48" placeholder="Digite uma mensagem"></textarea>
+                </div>
+
+                <button class="mt-6 w-full transform rounded-md bg-main-500 px-6 py-3 text-sm font-medium capitalize tracking-wide text-white transition-colors duration-300 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50">Enviar</button>
+            </form>
+            </div>
+        </div>
     </div>
+
+
+
+
 
 </x-app-layout>
