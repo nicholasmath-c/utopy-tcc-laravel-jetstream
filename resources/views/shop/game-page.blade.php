57586cfa4d96b6661117fe93748ee8b554ec6559
Etec <gustavo.silva1667@etec.sp.gov.br>
2023-06-21 20:41:25 -0300

    Update game-page.blade.php

--- resources/views/shop/game-page.blade.php
+++ resources/views/shop/game-page.blade.php
@@ -29,37 +29,30 @@
                     <p class="w-full"> {{ $game->genreGame->name }} </p>
                 </div>
                 <div class="mt-10">
-                    @if (App\Models\Shopcart::jogoNoCarrinho($game->id))
-                        <form action="{{ route('shopcart.store', $game->id) }}" method="post">
-                            @csrf
+                    @if (!App\Models\Shopcart::jogoNoCarrinho($game->id))
+                    <form action="{{ route('shopcart.store', $game->id) }}" method="post">
+                        @csrf
 
-                            <x-input
-                                type="hidden"
-                                name="game_id"
-                                value="{{ $game->id }}" />
+                        <x-input
+                            type="hidden"
+                            name="game_id"
+                            value="{{ $game->id }}" />
 
-                            <x-label class="text-white text-lg">
-                                Quantidade:
-                            </x-label>
-                            <x-input
-                                type="hidden"
-                                name="quantity"
-                                value="1"
-                                required
-                            />
+                        <x-label class="text-white text-lg" style="display: none;">
+                            Quantidade:
+                        </x-label>
+                        <x-input
+                            type="hidden"
+                            name="quantity"
+                            value="1"
+                            required
+                        />
 
-                            <x-button class="w-full h-16">
-                                <i class="fa fa-cart-plus" aria-label="Adicionar ao Carrinho" aria-hidden="true">
-                                </i>
-                            </x-button>
-                        </form>
-                        <x-button class="mt-4 w-64 h-16 text-white text-lg">
-                            Comprar Jogo
+                        <x-button class="w-full h-16">
+                            <i class="fa fa-cart-plus" aria-label="Adicionar ao Carrinho" aria-hidden="true">
+                            </i>
                         </x-button>
-                    @else
-                        <a class="mt-4 w-64 h-16 text-white text-lg" href="{{ route('shopcart.index') }}">
-                            Carrinho
-                        </a>
+                    </form>
                     @endif
                 </div>
             </div>
