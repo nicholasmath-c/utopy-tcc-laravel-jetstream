fe866a72550afb6e03f62049cee81619021c842a
ribeiroronald <ribeiroronald627@gmail.com>
2023-06-20 21:00:23 -0300

    Deivid- frontend new carousel

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class=" border-2 relative ">
+    <div class="  relative ">
 
         <div class=" relative ">
             <div class=" ">
@@ -9,47 +9,50 @@
                 </div>
             </div>
 
-            <div class=" flex flex-col-3 justify-between w-full absolute top-36 border-2  ">
-                <div class="w-96 ml-8 border-4 border-red-600 backdrop-blur-md bg-slate-500 bg-opacity-30">
-                    <img src="\games\Cuphead\Cuphead-Cover.png" alt="">
+            <div class=" flex flex-col-3 items-center w-full absolute top-36   ">
+                <div class=" w-[440px] ml-8">
+                    <img class=" rounded-lg " src="\games\Cuphead\Cuphead-Cover.png" alt="">
                 </div>
 
-                <div class="border-2 w-96 backdrop-blur-md bg-slate-800 bg-opacity-80">
+                <div class=" w-3/5 h-96 pl-5 pt-5 backdrop-blur-md bg-slate-800 bg-opacity-80">
                     <div>
                         <h1 class=" font-heading text-4xl"> <span> Cuphead </span></h1>
                     </div>
-                    <div class=" grid grid-cols-3 gap-x-1 p-1">
-                        <div class=" w-full py-[1px] border-2 border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
+                    <div class=" mt-3 grid grid-cols-3 gap-x-1 gap-y-1 p-1 w-80">
+                        <div class=" w-full py-[1px]  border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
                             <p class="text-center">Aventura</p>
                         </div>
-                        <div class=" w-full py-[1px] border-2 border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
+                        <div class=" w-full py-[1px]  border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
                             <p class="text-center">Plataforma</p>
                         </div>
-                        <div class=" w-full py-[1px] border-2 border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
+                        <div class=" w-full py-[1px]  border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
                             <p class="text-center">Bullet Hell</p>
                         </div>
-                        <div class=" w-full py-[1px] border-2 border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
+                        <div class=" w-full py-[1px]  border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
                             <p class="text-center">Bullet Hell</p>
                         </div>
-                        <div class=" w-full py-[1px] border-2 border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
+                        <div class=" w-full py-[1px]  border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
                             <p class="text-center">Aventura</p>
                         </div>
-                        <div class=" w-full py-[1px] border-2 border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
+                        <div class=" w-full py-[1px]  border-slate-600 rounded-xl bg-slate-800 bg-opacity-80 hover:bg-main-600 hover:cursor-pointer duration-200">
                             <p class="text-center">Aventura</p>
                         </div>
 
+                    </div>
+
+                    <div class=" mt-10">
+                        Em "Cuphead", embarque em uma aventura desafiadora e estilizada inspirada nos desenhos animados da década de 1930.
+                    </div>
+
+                    <div class=" flex flex-row w-full h-36 justify-end items-end pr-6 pb-4  ">
+                        <div>
+                            <x-button class="w-52 h-14 text-[18.5px]   "> Ver jogo  </x-button>
+                        </div>
                     </div>
 
                 </div>
 
-                <div class="flex flex-col w-96 mr-8 border-4 border-red-600 backdrop-blur-md bg-slate-800 bg-opacity-80">
-                    <div class="w-96">
-                        <h1 class=" font-heading "> Mim De Papai 2 bluetooth </h1>
-                    </div>
-                    <div class=" w-full h-full justify-end items-end ">
-                        <x-button> Ir para pagina </x-button>
-                    </div>
-                </div>
+
 
             </div>
 
@@ -72,37 +75,37 @@
     <div class=" sticky top-0 flex flex-row justify-center p-3">
         <div class=" p-2 px-[25px] flex flex-row bg-slate-700 bg-opacity-20 rounded-full shadow-md ">
             <div
-                class=" p-1 border-2 border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
+                class=" p-1  border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
                 Ação
             </div>
 
             <div
-                class=" p-1 border-2 border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
+                class=" p-1  border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
                 Gratuitos
             </div>
 
             <div
-                class=" p-1 border-2 border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
+                class=" p-1  border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
                 FPS
             </div>
 
             <div
-                class=" p-1 border-2 border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
+                class=" p-1  border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
                 Aventura
             </div>
 
             <div
-                class=" p-1 border-2 border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
+                class=" p-1  border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
                 8-Bits
             </div>
 
             <div
-                class=" p-1 border-2 border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
+                class=" p-1  border-slate-600 rounded-xl mx-1 my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
                 8-Bits
             </div>
 
             <div
-                class=" p-1 border-2 border-slate-600 rounded-xl mx-1  my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
+                class=" p-1  border-slate-600 rounded-xl mx-1  my-[1px] px-3 bg-slate-500 bg-opacity-20 hover:bg-main-600 hover:cursor-pointer duration-200">
                 8-Bits
             </div>
         </div>
@@ -111,7 +114,7 @@
     <div class="flex flex-col gap-4">
         @foreach ($game as $games)
             <div
-                class="bg-slate-800 flex flex-row items-center shadow-lg mx-16 rounded-md border-2 h-40 p-1 border-slate-800 hover:border-red-500 ease-in-out hover:cursor-pointer hover:transform hover:scale-[102%] duration-300 ">
+                class="bg-slate-800 flex flex-row items-center shadow-lg mx-16 rounded-md  h-40 p-1 border-slate-800 hover:border-red-500 ease-in-out hover:cursor-pointer hover:transform hover:scale-[102%] duration-300 ">
 
                 <div class=" h-36 w-44 ">
                     <img class=" h-36 w-44 rounded-md " src="{{ url("storage/games/$games->title/$games->cover") }}"
