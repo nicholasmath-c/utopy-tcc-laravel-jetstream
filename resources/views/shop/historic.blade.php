a437c6fa6354297f6293aa0ff64bfcb5a0ce3f47
Deivid Lima Souza <110212061+WaterCooler727@users.noreply.github.com>
2023-06-22 17:35:52 -0300

    about e historic

--- resources/views/shop/historic.blade.php
+++ resources/views/shop/historic.blade.php
@@ -1,28 +1,33 @@
 <x-app-layout>
-    <h2>Minhas Compras</h2>
+    <h2 class=" font-heading text-3xl mb-5">Minhas Compras</h2>
 
-    <table>
-        <tr>
-            <th>Data Compra</th>
-            <th>Situação</th>
-            <th>Jogo</th>
-            <th>Valor</th>            
-        </tr>
-        @if(isset($$lista_pedidos) && isset($lista_itens))
+    <table class="w-full">
+        <thead>
+            <tr
+                class="text-xs font-semibold tracking-wide text-left uppercase border-b border-slate-700 text-slate-400 bg-slate-900">
+                <th class="px-4 py-3">Data de lançamento</th>
+                <th class="px-4 py-3">Situação</th>
+                <th class="px-4 py-3">Jogo</th>
+                <th class="px-4 py-3">Valor</th>
+            </tr>
+        </thead>
+        <tbody class="alldata divide-y divide-slate-700 bg-slate-800">
+            @if (isset($$lista_pedidos) && isset($lista_itens))
             @foreach ($lista_pedidos as $pedido)
+            <tr class="bg-slate-700 text-slate-100">
+                <td class="px-4 py-3">{{ $pedido->getDataPedidoFormatada() }}</td>
+                <td class="px-4 py-3">{{ $pedido->statusDescricao() }}</td>
+            </tr>
+                @endforeach
+                @foreach ($lista_itens as $item)
                 <tr>
-                    <td>{{ $pedido->getDataPedidoFormatada() }}</td>
-                    <td>{{ $pedido->statusDescricao() }}</td>
-                </tr>
-            @endforeach
-            @foreach ($lista_itens as $item)
-                <tr>
-                    <td>{{ $item->title) }}</td>
-                    <td>{{ $item->valorItem }}</td>
+                    <td class="px-4 py-3">{{ $item->title }}</td>
+                    <td class="px-4 py-3">{{ $item->valorItem }}</td>
                 </tr>
             @endforeach
         @endif
-
-        Sem  Itens
+        <p class=" text-xl text-main-400">Sem items</p>
     </table>
+
+
 </x-app-layout>
