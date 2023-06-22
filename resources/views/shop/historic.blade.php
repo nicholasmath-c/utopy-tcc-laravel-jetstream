8ece9bdbe474ed20dfd1f30bb57244e0f1dc0609
Gustavo H. J <70487766+MrNullus@users.noreply.github.com>
2023-06-22 13:29:10 -0300

    Update historic.blade.php

--- resources/views/shop/historic.blade.php
+++ resources/views/shop/historic.blade.php
@@ -11,15 +11,6 @@
             <tr>
                 <td>{{ $pedido->getDataPedidoFormatada() }}</td>
                 <td>{{ $pedido->statusDescricao() }}</td>
-                <td>
-                    <div class="w-full flex justify-center py-12" id="button">
-                        <button
-                            class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 mx-auto transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-4 sm:px-8 py-2 text-xs sm:text-sm info-compra"
-                            onclick="modalHandler(true)" data-value="{{ $pedido->id }}">
-                            <i class="fas fa-shopping-basket"></i>
-                        </button>
-                    </div>
-                </td>
             </tr>
         @endforeach
     </table>
